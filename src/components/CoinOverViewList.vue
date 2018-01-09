7b22550cddf1334fba61f0bca446e028f8347cc9
biigpongsatorn <biig_pongsatorn@hotmail.com>
2018-01-09 23:49:34 +0700

    Crypto overview bar smoothly

---
+++ src/components/CoinOverViewList.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="coin-overview--container">
+    <div class="coin-overview--li-box">
+      <img class="coin-overview--coin-img" src="https://files.coinmarketcap.com/static/img/coins/32x32/bitcoin.png">
+      <div class="coin-overview--coin-box">
+        <div class="coin-overview--coin-name">
+          Bitcoin (BTC)
+        </div>
+        <div class="coin-overview--coin-detail">
+          $16,815.10 
+          <span class="has-text-success">(11.68%)</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="coin-overview--li-box">
+      <img class="coin-overview--coin-img" src="https://files.coinmarketcap.com/static/img/coins/32x32/ripple.png">
+      <div class="coin-overview--coin-box">
+        <div class="coin-overview--coin-name">
+          Ripple (XRP)
+        </div>
+        <div class="coin-overview--coin-detail">
+          $2.91
+          <span class="has-text-danger">(-19.18%)</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="coin-overview--li-box">
+      <img class="coin-overview--coin-img" src="https://files.coinmarketcap.com/static/img/coins/32x32/ethereum.png">
+      <div class="coin-overview--coin-box">
+        <div class="coin-overview--coin-name">
+          Ethereum (ETH)
+        </div>
+        <div class="coin-overview--coin-detail">
+          $1000.83
+          <span class="has-text-success">(19.18%)</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="coin-overview--li-box">
+      <img class="coin-overview--coin-img" src="https://files.coinmarketcap.com/static/img/coins/32x32/stellar.png">
+      <div class="coin-overview--coin-box">
+        <div class="coin-overview--coin-name">
+          Stellar (XLM)
+        </div>
+        <div class="coin-overview--coin-detail">
+          $0.77509
+          <span class="has-text-success">(12.98%)</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="coin-overview--li-box">
+      <img class="coin-overview--coin-img" src="https://files.coinmarketcap.com/static/img/coins/32x32/kyber-network.png">
+      <div class="coin-overview--coin-box">
+        <div class="coin-overview--coin-name">
+          Kyber Network (KNC)
+        </div>
+        <div class="coin-overview--coin-detail">
+          $3.07
+          <span class="has-text-danger">(-11.93%)</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="coin-overview--li-box">
+      <img class="coin-overview--coin-img" src="https://files.coinmarketcap.com/static/img/coins/32x32/etherparty.png">
+      <div class="coin-overview--coin-box">
+        <div class="coin-overview--coin-name">
+          Etherparty (FUEL)
+        </div>
+        <div class="coin-overview--coin-detail">
+          $0.321365
+          <span class="has-text-danger">(-19.79%)</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="coin-overview--li-box">
+      <img class="coin-overview--coin-img" src="https://files.coinmarketcap.com/static/img/coins/32x32/blockmason.png">
+      <div class="coin-overview--coin-box">
+        <div class="coin-overview--coin-name">
+          BlockMason Credit Protocol (BCPT)
+        </div>
+        <div class="coin-overview--coin-detail">
+          $1.15
+          <span class="has-text-success">(66.50%)</span>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CoinOverViewList'
+}
+</script>
+
+<style scoped>
+.coin-overview--container {
+  float: left;
+  width: 50%;
+  display: flex;
+  justify-content: space-between;
+}
+.coin-overview--li-box {
+  float: left;
+  padding: 0 20px;
+}
+.coin-overview--coin-img {
+  float: left;
+  margin-top: 5px;
+  width: 32px;
+  height: 32px;
+}
+.coin-overview--coin-box {
+  float: left;
+  padding-left: 10px;
+}
+.coin-overview--coin-name {
+  font-weight: bold;
+}
+.coin-overview--coin-detail {
+  font-size: 11px;
+}
+</style>
--- src/components/CryptoOverView.vue
+++ src/components/CryptoOverView.vue
@@ -1,135 +1,46 @@
 <template>
-  <div class="stretch overview--container">
-
-    <div class="overview--li-box">
-      <img class="overview--coin-img" src="https://files.coinmarketcap.com/static/img/coins/32x32/bitcoin.png">
-      <div class="overview--coin-box">
-        <div class="overview--coin-name">
-          Bitcoin (BTC)
-        </div>
-        <div class="overview--coin-detail">
-          $16,815.10 
-          <span class="has-text-success">(11.68%)</span>
-        </div>
-      </div>
+  <div class="overview--container">
+    <div class="overview--slide">
+      <CoinOverViewList/>
+      <CoinOverViewList/>
     </div>
-
-    <div class="overview--li-box">
-      <img class="overview--coin-img" src="https://files.coinmarketcap.com/static/img/coins/32x32/ripple.png">
-      <div class="overview--coin-box">
-        <div class="overview--coin-name">
-          Ripple (XRP)
-        </div>
-        <div class="overview--coin-detail">
-          $2.91
-          <span class="has-text-danger">(-19.18%)</span>
-        </div>
-      </div>
-    </div>
-
-    <div class="overview--li-box">
-      <img class="overview--coin-img" src="https://files.coinmarketcap.com/static/img/coins/32x32/ethereum.png">
-      <div class="overview--coin-box">
-        <div class="overview--coin-name">
-          Ethereum (ETH)
-        </div>
-        <div class="overview--coin-detail">
-          $1000.83
-          <span class="has-text-success">(19.18%)</span>
-        </div>
-      </div>
-    </div>
-
-    <div class="overview--li-box">
-      <img class="overview--coin-img" src="https://files.coinmarketcap.com/static/img/coins/32x32/stellar.png">
-      <div class="overview--coin-box">
-        <div class="overview--coin-name">
-          Stellar (XLM)
-        </div>
-        <div class="overview--coin-detail">
-          $0.77509
-          <span class="has-text-success">(12.98%)</span>
-        </div>
-      </div>
-    </div>
-
-    <div class="overview--li-box">
-      <img class="overview--coin-img" src="https://files.coinmarketcap.com/static/img/coins/32x32/kyber-network.png">
-      <div class="overview--coin-box">
-        <div class="overview--coin-name">
-          Kyber Network (KNC)
-        </div>
-        <div class="overview--coin-detail">
-          $3.07
-          <span class="has-text-danger">(-11.93%)</span>
-        </div>
-      </div>
-    </div>
-
-    <div class="overview--li-box">
-      <img class="overview--coin-img" src="https://files.coinmarketcap.com/static/img/coins/32x32/etherparty.png">
-      <div class="overview--coin-box">
-        <div class="overview--coin-name">
-          Etherparty (FUEL)
-        </div>
-        <div class="overview--coin-detail">
-          $0.321365
-          <span class="has-text-danger">(-19.79%)</span>
-        </div>
-      </div>
-    </div>
-
-    <div class="overview--li-box">
-      <img class="overview--coin-img" src="https://files.coinmarketcap.com/static/img/coins/32x32/blockmason.png">
-      <div class="overview--coin-box">
-        <div class="overview--coin-name">
-          BlockMason Credit Protocol (BCPT)
-        </div>
-        <div class="overview--coin-detail">
-          $1.15
-          <span class="has-text-success">(66.50%)</span>
-        </div>
-      </div>
-    </div>
-
   </div>
 </template>
 
 <script>
+import CoinOverViewList from '@/components/CoinOverViewList'
 export default {
-  name: 'CryptoOverview'
+  name: 'CryptoOverview',
+  components: {
+    CoinOverViewList
+  }
 }
 </script>
 
 <style scoped>
+@keyframes moveSlideshow {
+  100% { 
+    transform: translateX(-50%);  
+  }
+}
 .overview--container {
+  transform: translate3d(0, 0, 0); /* Hey browser, use your GPU */
   height: 60px;
   background-color: #ffffff;
   box-shadow: 0 1px 0 0 rgba(0, 0, 0, 0.06);
   margin-bottom: 20px;
-  display: flex;
-  justify-content: space-between;
+  position: relative;
   padding: 10px;
+  overflow: hidden;
 }
-.overview--li-box {
-  float: left;
-  padding: 0 20px;
-}
-.overview--coin-img {
-  float: left;
-  margin-top: 5px;
-  width: 32px;
-  height: 32px;
-}
-.overview--coin-box {
-  float: left;
-  padding-left: 10px;
-}
-.overview--coin-name {
-  font-weight: bold;
-}
-.overview--coin-detail {
-  font-size: 11px;
+.overview--slide {
+  transform: translate3d(0, 0, 0); /* Hey browser, use your GPU */
+  height: 50px;
+  width: 200%;
+  position: absolute;
+  top: 10px;
+  left: 0;
+  animation: moveSlideshow 12s linear infinite !important;
 }
 </style>
 
